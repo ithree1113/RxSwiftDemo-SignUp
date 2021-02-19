ccaf40da157ea448f3dbe646388b9588e8578931
ithree1113 <ithree1113@gmail.com>
2021-02-19 16:01:30 +0800

    Add SceneDelegateCoordinator to show the root view

---
+++ RxSwiftDemo-SignUp/Coordinator/SceneDelegateCoordinator.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegateCoordinator.swift
+//  RxSwiftDemo-SignUp
+//
+//  Created by YuHsiang on 2021/2/19.
+//
+
+import UIKit
+
+class SceneDelegateCoordinator: Coordinator {
+    var children: [Coordinator] = []
+    let router: Router
+    
+    //MARK: - Life cycle
+    init(router: Router) {
+        self.router = router
+    }
+    
+    //MARK: - Method
+    func present(animated: Bool, onDismissed: (() -> Void)?) {
+        let vc = UIViewController(nibName: nil, bundle: nil)
+        vc.view.backgroundColor = .blue
+        
+        let navigationController = UINavigationController(rootViewController: vc)
+        navigationController.setTransparentStyle()
+        router.present(navigationController, animated: false)
+    }
+}
